2c447494801bdc8b3b7d30e041980fac83c47b71
pwnzr <pvvner@yandex.ru>
2017-04-14 02:33:06 +0300

    Set up router with express.Router()

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 // set public directory as static directory
 // whatever we put here, will be used by express first, before it does an. else
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-
 app.set('view engine', 'ejs');
+
+// Books/
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.send('Hello Books!');
+    });
+// /Books/single
+bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Book!');
+    });
+// for /Books use bookRouter
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res) {
             res.render('index', {
