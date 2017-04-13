af6209811d23638f0ef192f7f3c63d42f40018c3
pwnzr <pvvner@yandex.ru>
2017-04-13 15:39:23 +0300

    Add static files

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var app = express();
 
 var port = 5000;
 
+// set public directory as static directory
+// whatever we put here, will be used by express first, before it does an. else
+app.use(express.static('public'));
+
+// src/views/index.html -> index.html
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res) {
     res.send('Hello World');
 });
