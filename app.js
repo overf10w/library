56a4e3027703825bbec5c86dc8e7cdd2257ce2ca
pwnzr <pvvner@yandex.ru>
2017-04-14 17:48:10 +0300

    Move bookRouter to a separate file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
-var bookRouter = express.Router();
+var bookRouter = require('./src/routes/bookRoutes');
 
 // set public directory as static directory
 // whatever we put here, will be used by express first, before it does an. else
@@ -12,58 +12,6 @@ app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
-var books = [
-        {
-            title: 'War And Peace',
-            genre: 'Historical Fiction',
-            author: 'Lev Nikolaevich Tolstoy',
-            read: false
-        },
-        {
-            title: 'Les Miserables',
-            genre: 'Historical Fiction',
-            author: 'Victor Hugo',
-            read: false
-        },
-        {
-            title: 'The Time Machine',
-            genre: 'Science Fiction',
-            author: 'H. G. Wells',
-            read: false
-        },
-        {
-            title: 'A Journey into the Center of the Earth',
-            genre: 'Science fiction',
-            author: 'Jules Verne',
-            read: false
-        },
-        {
-            title: 'The dark world',
-            genre: 'Historical Fiction',
-            author: 'John Doe',
-            read: false
-        }
-];
-// Books/
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('books', {
-            title: 'Books',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            }, {
-                Link:'/Authors',
-                Text: 'Authors'
-            }],
-            books: books
-        });
-    });
-// /Books/single
-bookRouter.route('/single')
-    .get(function (req, res) {
-        res.send('Hello Single Book!');
-    });
 // for /Books use bookRouter
 app.use('/Books', bookRouter);
 
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,58 @@
+var express = require('express');
+
+var bookRouter = express.Router();
+
+var books = [
+        {
+            title: 'War And Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolaevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Miserables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false
+        },
+        {
+            title: 'The Time Machine',
+            genre: 'Science Fiction',
+            author: 'H. G. Wells',
+            read: false
+        },
+        {
+            title: 'A Journey into the Center of the Earth',
+            genre: 'Science fiction',
+            author: 'Jules Verne',
+            read: false
+        },
+        {
+            title: 'The dark world',
+            genre: 'Historical Fiction',
+            author: 'John Doe',
+            read: false
+        }
+];
+// Books/
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.render('books', {
+            title: 'Books',
+            nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            }, {
+                Link:'/Authors',
+                Text: 'Authors'
+            }],
+            books: books
+        });
+    });
+// /Books/single
+bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Book!');
+    });
+
+module.exports = bookRouter;
