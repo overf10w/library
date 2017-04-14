5e7164bf21329a6db806cfb65ca885cfd02e69d7
pwnzr <pvvner@yandex.ru>
2017-04-15 01:38:39 +0300

    Populate mongodb collection with books data

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,55 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var books = [
+        {
+            title: 'War And Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolaevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Miserables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false
+        },
+        {
+            title: 'The Time Machine',
+            genre: 'Science Fiction',
+            author: 'H. G. Wells',
+            read: false
+        },
+        {
+            title: 'A Journey into the Center of the Earth',
+            genre: 'Science fiction',
+            author: 'Jules Verne',
+            read: false
+        },
+        {
+            title: 'The dark world',
+            genre: 'Historical Fiction',
+            author: 'John Doe',
+            read: false
+        }
+];
 
 var router = function (nav) {
     adminRouter.route('/addBooks')
-        .get(function(req, res) {
-            res.send('inserting books...');
+        .get(function (req, res) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books'); // .collection() returns new col. instance
+                // populate mongodb with mock data
+                // results: hand us the same arr (of books), but with mongodb stuff associated with it
+                collection.insertMany(books, function(err, results) {
+                    res.send(results);
+                    db.close();
+                });
+            });
+            // TODO ! don't send 2 responses
+            // res.send('inserting books...');
         });
     return adminRouter;
 };
