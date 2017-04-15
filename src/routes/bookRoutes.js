0db6cf0194cc59ac801457a4b7296abb42c45dd4
pwnzr <pvvner@yandex.ru>
2017-04-15 04:00:10 +0300

    Sel. book & go to its page (now books have funny id)

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function (nav) {
-
     // Books/
     bookRouter.route('/')
         .get(function (req, res) {
             var url = 'mongodb://localhost:27017/libraryApp';
+
             mongodb.connect(url, function (err, db) {
                 var collection = db.collection('books');
-                collection
-                    .find({})
+                // find just everything
+                collection.find({})
                     .toArray(function (err, results) {
                         res.render('bookListView', {
                             title: 'Books',
@@ -24,11 +25,20 @@ var router = function (nav) {
     // /Books/:id
     bookRouter.route('/:id')
         .get(function (req, res) {
-            var id = req.params.id;
-            res.render('bookView', {
-                title: 'Books',
-                nav: nav,
-                book: books[id]
+            // items in mongo db have special type of id
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                // find the one whose _id mathces id
+                collection.findOne({_id: id}, function (err, results) {
+                        res.render('bookView', {
+                            title: 'Books',
+                            nav: nav,
+                            book: results
+                        });
+                    });
             });
         });
     return bookRouter;
