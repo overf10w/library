ba9cad3d0b0ed841f9319756479228f851fd5afa
pwnzr <pvvner@yandex.ru>
2017-04-16 05:30:22 +0300

    Create auth route. Use bodyparser to parse request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -15,10 +16,16 @@ var nav = [
 ];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 // set public directory as static directory
 // whatever we put here, will be used by express first, before it does an. else
 app.use(express.static('public'));
+// Check if there's body in coming JSON, and if it is, create req.body
+app.use(bodyParser.json());
+// Same as bodyParser.json() but for urlencoded strings
+app.use(bodyParser.urlencoded());
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
@@ -26,6 +33,7 @@ app.set('view engine', 'ejs');
 // for /Books use bookRouter
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function(req, res) {
             res.render('index', {
