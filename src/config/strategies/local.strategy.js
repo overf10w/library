60af673189b36db8ae9079b64f3531a299ad183b
pwnzr <pvvner@yandex.ru>
2017-04-17 03:31:46 +0300

    Verifying user in database

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
 
 module.exports = function () {
     passport.use(new LocalStrategy(
@@ -12,11 +13,21 @@ module.exports = function () {
     },
     // func (to verify user credentials)
     function(username, password, done) {
-        var user = {
-            username: username,
-            password: password
-        };
-        // verify callback: supply Passport with user that authenticated (read docs)
-        done(null, user);
+        var url = 'mongodb://localhost:27017/libraryApp';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('users');
+            collection.findOne({
+                username: username
+            },
+            function (err, results) {
+                if (results.password === password) {
+                    var user = results;
+                    done(null, user);
+                } else {
+                    done(null, false, {message: 'Bad password'});
+                }
+            });
+        });
     }));
 };
